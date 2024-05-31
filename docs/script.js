07b277cd0d4b836055e639e763289692b62c68ca
Max <max.pcgt@gmail.com>
2024-05-31 20:49:48 +0200

    Updates files

--- docs/script.js
+++ docs/script.js
@@ -1,28 +1,27 @@
-const passwordBox = document.getElementById("password")
-const lenght = 12
+const passwordBox = document.getElementById("password");
+const lenght = 12;
 
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWYZ"
-const lowerCase = "abcdefghijklmnopqrstuvwxyz"
-const number = "0123456789"
-const symbol = "@#$%^&*()_+~|}{[]></-="
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
 
-const allChars = upperCase + lowerCase + number + symbol
-    
+const allChars = upperCase + lowerCase + number + symbol;
 
 function createPassword() {
-    let password = ""
-    password += upperCase[Math.floor(Math.random() * upperCase.length)]
-    password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
-    password += number[Math.floor(Math.random() * number.length)]
-    password += symbol[Math.floor(Math.random() * symbol.length)]
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
 
-    while(lenght > password.length) {
-        password += allChars[Math.floor(Math.random() * allChars.length)]
-    }
-    passwordBox.value = password
+  while (lenght > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
 }
 
 function copyPassword() {
-    passwordBox.select()
-    document.execCommand("copy")
+  passwordBox.select();
+  document.execCommand("copy");
 }
