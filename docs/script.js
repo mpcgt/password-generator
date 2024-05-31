5353102e17083f0835588a3cd75e57fd402a3ad2
Max <max.pcgt@gmail.com>
2024-05-31 20:38:46 +0200

    Update file

--- docs/script.js
+++ docs/script.js
@@ -5,3 +5,11 @@ const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWYZ"
 const loverCase = "abcdefghijklmnopqrstuvwxyz"
 const number = "0123456789"
 const symbol = "@#$%^&*()_+~|}{[]></-="
+
+function createPassword() {
+    let password = ""
+    password += upperCase[Math.floor(Math.random() * upperCase.length)]
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+    password += number[Math.floor(Math.random() * number.length)]
+    password += symbol[Math.floor(Math.random() * symbol.length)]
+}
