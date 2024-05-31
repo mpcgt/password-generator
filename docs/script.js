0be9972f243abd0a46343b68b0a2e872697aea05
Max <max.pcgt@gmail.com>
2024-05-31 20:36:35 +0200

    Update file

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,7 @@
 const passwordBox = document.getElementById("password")
 const lenght = 12
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWYZ"
+const loverCase = "abcdefghijklmnopqrstuvwxyz"
+const number = "0123456789"
+const symbol = "@#$%^&*()_+~|}{[]></-="
